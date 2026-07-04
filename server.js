3d99cac4ad66acbee32a914cd11e11203f211d90
twbmyny803-png <twbmyny803@gmail.com>
2026-07-04 22:33:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(__dirname));
+
 app.get("/", (req, res) => {
-  res.send("مرحبًا بك في موقع ibok-online");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(PORT, () => {
